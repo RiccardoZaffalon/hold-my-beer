fa06f6bff45c18925af43068b514d11e2c80694e
Riccardo Zaffalon <rzaffalon@alpenite.com>
2020-10-08 15:09:42 +0200

    Remove eslit, it was already included you dummy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import styled from 'styled-components';
-import { colors } from '../styles/variables';
+import React from "react";
+import styled from "styled-components";
+
+import { colors } from "../styles/variables";
 
 const Header = styled.header`
-    background: ${colors.background.secondary};
-    color: ${colors.foreground.primary};
+  background: ${colors.background.secondary};
+  color: ${colors.foreground.primary};
 `;
 
 export default function () {
-    return(
-        <Header>
-            Header here
-        </Header>
-    )
+  return <Header>Header here</Header>;
 }
